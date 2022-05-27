0c27f5458b6847448b88995296cf055aac0937d1
robb dunlap <robb@altana.ai>
2022-05-27 16:35:09 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='standard_helpers',
+    version='0.0.1',
+    author='Robb Dunlap',
+    author_email='robb@altana.ai',
+    description='testing pip installation of github packages',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/robb-altoid/standard_helpers',
+    project_urls = {
+        "Bug Tracker": "https://github.com/robb-altoid/standard_helpers/issues"
+    },
+    license='MIT',
+    packages=['standard_helpers'],
+    install_requires=['typing','pyspark','databrick','pandas'],
+)
