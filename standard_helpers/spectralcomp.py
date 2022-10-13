15f4f3dd1750c412dd7712d56724a38a13361b8a
robb dunlap <robb@altana.ai>
2022-10-13 15:10:27 -0400

    minor changes to spectralcomp

--- standard_helpers/spectralcomp.py
+++ standard_helpers/spectralcomp.py
@@ -14,7 +14,8 @@ def magnify():
 
 def HS_Simple_Comp(A,B, Table):
   '''
-  Takes two rows, defined by index values (A and B) from a table with HS code columns. Provides the cosine simliarity of A   and B. This measures directional simliarity in cartesean vector space. 
+  Takes two rows, defined by index values (A and B) from a table with HS code columns. Provides the 
+  cosine simliarity of A and B. This measures directional simliarity in cartesean vector space. 
   '''
   Table = spark.read.table(str(Table)).toPandas()
   num_df = (Table.iloc[[A,B]]).select_dtypes(include = ['int64'])
@@ -37,7 +38,8 @@ def HS_Cosine_Matrix(Table):
 
 def hs_simple_comp_from_pdf(A,B, Table):
   '''
-  Takes two rows, defined by index values (A and B) from a table with HS code columns. Provides the cosine simliarity of A   and B. This measures directional simliarity in cartesean vector space. 
+  Takes two rows, defined by index values (A and B) from a table with HS code columns. Provides the 
+  cosine simliarity of A and B. This measures directional simliarity in cartesean vector space. 
   '''
   score = cosine_similarity(Table.iloc[[A,B]])[0,1]
   return score
@@ -46,11 +48,15 @@ def hs_cosine_matrix_from_pdf(Table):
   '''
   Takes a table with HS codes and produces a cosine simliarity of A and B. This measures directional
   simliarity in cartesean vector space. 
+  
+  Args:
+      Table (pandas.DataFrame): the df containing the raw HS specta (not normalized). The table should 
+      have the spectra for each entity in a row (tidy data)
   '''
 
   matrix = pd.DataFrame(cosine_similarity(Table))
 
   return matrix.style.background_gradient(cmap='coolwarm', axis=1)\
-    .set_properties(**{'max-width': '80px', 'font-size': '8pt'})\
-    .set_caption("Hover to magify")
+    .set_properties(**{'max-width': '80px', 'font-size': '8pt'})
+    # .set_caption("Hover to magify")
     # .set_table_styles(magnify())
