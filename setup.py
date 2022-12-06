c05d967d9fd8d9d8a8219a1b35d1745eccb5da61
robb dunlap <robb@altana.ai>
2022-12-06 12:27:00 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='standard_helpers',
-    version='0.1.0',
+    version='0.1.1',
     author='Robb Dunlap',
     author_email='robb@altana.ai',
     description='databricks quick query method plus swagger api v3 access functions',
